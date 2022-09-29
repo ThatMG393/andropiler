a83f864723ca33e57b226c77f968fac32d8cffde
ThatMG393 <thatmg393@gmail.com>
2022-09-29 15:04:50 +0800

    Major changes in 'make.sh'

--- make.sh
+++ make.sh
@@ -1,22 +1,55 @@
 
-set -e # Stop on error
+# shopt -s extglob # Much more powerful pattern matching
+
+infoClr="\033[1;34m"
+warnClr='\033[1;33m'
+errClr="\033[1;31m"
+defClr="\033[0m"
+info() { echo -e "${infoClr}I: ${*}${defClr}";:; }
+warn() { echo -e "${warnClr}W: ${*}${defClr}";:;}
+err() { echo -e "${errClr}E: ${*}${defClr}"; exit 1;:; }
+
+FLAVOR="unknown"
+
+# If no VALID flavor has been found, we will fallback to 'debug'
+getFlavor() {
+    BLD_GRDL=$( sed -n -e '5,15 p' app/build.gradle )
+    FLAVOR="$( echo $BLD_GRDL | grep -Po "(debug)|(release)" )"
+
+    if [ ! -n "${FLAVOR##+([[:space:]])}" ]; then
+        warn "\$FLAVOR is empty, falling back to 'debug'"
+
+        replace=$( echo $BLD_GRDL | grep -Po 'def flavor = \"\b(?!\w*[debug][release]\w*)\w*\w*\b\"')
+        replacement='def flavor = "debug"'
+        sed -i "s|${replace}|${replacement}|" app/build.gradle
+
+        getFlavor # Call getFlavor again
+    else
+        info "Flavor : $FLAVOR"
+    fi
+}
+getFlavor
+
+set -e # Moving it here since the above line can return a non-zero exit codes
 
 GEN_FLDR=app/build/libs
 
-BLD_FLDR=dist/build
+BLD_FLDR="dist/build/$FLAVOR"
 BLD_MAIN_FLDR="$BLD_FLDR/main"
 
 PKG_FLDR=dist/pkg
 
 echo ""
-echo "Compiling to fat jar..."
-echo "If you wish to change the application build flavor"
-echo "Open app/build.gradle then edit 'flavor' near the top."
+info "Compiling to a $FLAVOR jar..."
+info "If you wish to change the application build flavor"
+info "Open app/build.gradle then edit 'flavor' near the top."
+info "Flavor can only be 'debug' or 'release'"
 echo ""
 # TODO: Add build flavor selection
+# Since we getFlavor() I think we can override it's fallback value!
 
 start() {
-    echo "Building..."
+    info "Building..."
     bash gradlew clean build
 
     rm -rf $BLD_FLDR
@@ -24,7 +57,7 @@ start() {
     mkdir -p $PKG_FLDR
     
     echo ""
-    echo "Copying app-*.jar to $BLD_MAIN_FLDR..."
+    info "Copying app-*.jar to '${BLD_MAIN_FLDR}'..."
     cp $GEN_FLDR/app-*.jar $BLD_MAIN_FLDR
     
     JAR_FNAME=$( basename -- $GEN_FLDR/app-*.jar )
@@ -33,17 +66,17 @@ start() {
     cp andropiler $BLD_FLDR
 
     echo ""
-    echo "Skipping 'zip' install."
+    info "Skipping 'zip' install."
     # echo "Installing 'zip'..."
     # apt install zip # Install ZIP (Not supported on Github Actions)
 
-    cd $BLD_FLDR
+    info "Creating ZIP file..."
+    zip -r andropiler$JAR_VER.zip $BLD_FLDR
+
     echo ""
-    echo "Creating ZIP file..."
-    zip -r andropiler$JAR_VER.zip ../build
-    mv andropiler$JAR_VER.zip ../pkg
-    cd ../../
+    info "Moving 'andropiler${JAR_VER}' to '${PKG_FLDR}'"
+    mv andropiler$JAR_VER.zip $PKG_FLDR
 }
 
 start
-echo "Done!"
+info "Done!"
